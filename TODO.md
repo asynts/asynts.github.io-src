7256c99305a0aee5c2c01a1755446a2de98bac79
asynts <asynts@gmail.com>
2018-09-30 14:30:21 +0200

    add prettify back in

--- TODO.md
+++ TODO.md
@@ -1,3 +1,4 @@
 # TODO
 
 - [ ] refactor indexer plugin
+- [ ] fill in the prettify welcome page
---
+++ src/pr/main.js
@@ -0,0 +1,33 @@
+const output = document.getElementById("output");
+const welcome = document.getElementById("welcome");
+const error = document.getElementById("error");
+
+const params = new URLSearchParams(window.location.search);
+
+if(window.location.hash.length > 0) {
+  const url = window.location.hash.substr(1);
+  const lang = params.get("lang");
+
+  if(lang !== null) {
+    output.classList.add(`lang-${lang}`);
+  }
+
+  const headers = new Headers();
+  headers.append("Accept", "text/plain");
+
+  const request = new Request(url, {
+    method: "GET",
+    cache: "no-cache",
+    headers
+  });
+
+  fetch(request)
+    .then(response => response.text())
+    .then(text => {
+      output.innerText = text;
+      console.log(PR);
+      PR.prettyPrint();
+    }).catch(err => err.innerText = err);
+} else {
+  welcome.hidden = false;
+}
