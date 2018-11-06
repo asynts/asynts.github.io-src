bdea31f716269cd66027cb840a2ea2e29289a41f
asynts <asynts@gmail.com>
2018-11-06 12:53:57 +0100

    another example

---
+++ src/so/53165372/0001.lua
@@ -0,0 +1,14 @@
+-- The lines #1 to #4 are equal.
+
+-- #1
+local a = false print(a and true or false)
+
+-- #2
+print(false and true or false)
+
+-- #3
+print(false or false)
+
+-- #4
+print(false)
+
