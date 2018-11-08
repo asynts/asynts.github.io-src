2448f4c8c5420e20b7ddb22ec00e0ac0a8823400
asynts <asynts@gmail.com>
2018-11-08 13:44:08 +0100

    untested

--- src/_plugins/downloader.rb
+++ src/_plugins/downloader.rb
@@ -2,30 +2,51 @@ require "uri"
 require "net/http"
 require "digest"
 
-module Downloader
-  # TODO: private
-  def _download uri, file
-    Net::HTTP.get_response uri do |resp|
-      resp.read_body do |chunk|
-        file.write chunk
+module Jekyll
+  class DownloadTag < Liquid::Tag
+    def initialize tag_name, url, tokens
+      super
+
+      @url = url
+      download @url
+    end
+
+    @@downloaded = []
+    def self.download url
+      fname = filename url
+
+      # don't download the file again
+      if @@downloaded.include? fname
+        return fname
       end
+
+      uri = URI url
+      file = open fname, "w"
+      begin
+        # download via http
+        Net::HTTP.get_response uri do |resp|
+          resp.read_body { |chunk| file.write chunk }
+        end
+      ensure
+        file.close()
+      end
+
+      @@downloaded << fname
+      fname
     end
-  end
 
-  def download url, ext=""
-    uri = URI url
-    name = Digest::SHA2.hexdigest url
-    name += ext
+    # generate a filename for the given url
+    def self.filename url
+      ext = ".js" # todo
+      dir = "/dw/"
+      name = Digest::SHA2.hexdigest url
 
-    file = open name, "w"
-
-    begin 
-      _download uri, file
-    ensure
-      file.close()
+      "#{dir}/#{name}#{ext}"
     end
 
-    name
+    def render ctx
+      return filename @url
+    end
   end
 end
 
