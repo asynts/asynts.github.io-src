2a3c1588492da3f63b30b813ec8c05dad1734d92
asynts <asynts@gmail.com>
2019-04-20 15:40:45 +0200

    dataset for prog2

---
+++ src/ref/0011_data.java
@@ -0,0 +1,14 @@
+public class Main {
+
+  // ...
+
+  static final int EPOCHS = 50;
+  static final double LEARN_RATE = 0.005;
+  static final double DATA_OUTPUT[] = {0, 0, 1, 0, 0, 1, 1, 1};
+  static final double DATA_INPUT[][] = {
+      {1, 1}, {1, 3}, {2, 7}, {3, 1}, {1, 3}, {5, 0}, {0, 5}, {4, 3},
+  };
+
+  // ...
+}
+
